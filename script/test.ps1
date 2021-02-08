64fd79b31f3ff922b438b6ac6551924a8d1ee1c4
Pete O'Shea <mail@peteoshea.co.uk>
2021-02-08 18:15:09 +0000

    Just run jest tests as part of the test script
    The linting etc. if forced when pushing code anyway and fails to run during the GitHub action as eslint isn't installed.
    Excluded node_modules when checking for bash scripts.

--- script/test.ps1
+++ script/test.ps1
@@ -15,18 +15,21 @@ if ($PSBoundParameters.ContainsKey('Test')) {
   & "$scriptPath\bin\checkScripts.ps1"
 }
 
-Write-Output "`n==> PowerShell script analysis finished at $(Get-Date)`n"
+Write-Output "`n==> PowerShell script analysis finished at $(Get-Date)"
 
 if ($LastExitCode) {
   Write-Output "`ncheckScripts failed with exit code: $LastExitCode"
   Exit $LastExitCode
 }
 
-Write-Output "==> Start running Node tests at $(Get-Date)"
-npm run test-all
-if ($LastExitCode) {
-  Write-Output "`nNode tests failed with exit code: $LastExitCode"
-  Exit $LastExitCode
+# Do not run Node tests during CI as will already have been run in the base script
+if ([System.Environment]::GetEnvironmentVariable("CI") -ne "true") {
+  Write-Output "`n==> Start running Node tests at $(Get-Date)"
+  npm run test
+  if ($LastExitCode) {
+    Write-Output "`nNode tests failed with exit code: $LastExitCode"
+    Exit $LastExitCode
+  }
 }
 
 Write-Output "`n==> Tests finished at $(Get-Date)`n"
