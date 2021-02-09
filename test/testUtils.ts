96b2dc742358cf9709bca9303fe13bf5f463cfa8
Pete O'Shea <mail@peteoshea.co.uk>
2021-02-09 17:36:53 +0000

    Fix some linting errors after updating eslint rules

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 // import { ThemeProvider } from "my-ui-lib"
 // import { TranslationProvider } from "my-i18n-lib"
 // import defaultStrings from "i18n/en-x-default"
@@ -14,7 +14,9 @@ const Providers = ({ children }) => {
   // )
 }
 
-const customRender = (ui, options = {}) => render(ui, { wrapper: Providers, ...options })
+const customRender = (ui: React.ReactElement, options = {}): RenderResult => {
+  return render(ui, { wrapper: Providers, ...options })
+}
 
 // re-export everything
 export * from '@testing-library/react'
