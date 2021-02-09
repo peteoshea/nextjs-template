5cd9cffac9b57fe27374cb68d1704848d80ba3c9
Pete O'Shea <mail@peteoshea.co.uk>
2021-02-09 11:58:06 +0000

    Improve scripts
    Run test-all so linting etc. is also run during ci tests.
    Just search for bash scripts in the script folder to speed things up a bit, this also simplifies the file check.
    Ensure scripts are being run from the correct location as some scripts required the directory to be changed as they expect to be in a specific folder.

--- script/test.ps1
+++ script/test.ps1
@@ -5,31 +5,40 @@ param(
   $Test
 )
 
-$scriptPath = Split-Path -Path $PSCommandPath -Parent
+function Undo-Location {
+  Pop-Location
+  Write-Output "Change directory back to: $(Get-Location)"
+}
 
 Write-Output "`n==> Start running tests at $(Get-Date)"
 
-if ($PSBoundParameters.ContainsKey('Test')) {
-  & "$scriptPath\bin\checkScripts.ps1" $Test
-} else {
-  & "$scriptPath\bin\checkScripts.ps1"
-}
+$scriptPath = Split-Path -Path $PSCommandPath -Parent
+$basePath = Split-Path -Path $scriptPath -Parent
+Push-Location "$basePath"
+Write-Output "Change directory to: $basePath"
 
+& "$scriptPath\bin\checkScripts.ps1"
 Write-Output "`n==> PowerShell script analysis finished at $(Get-Date)"
-
 if ($LastExitCode) {
+  Undo-Location
   Write-Output "`ncheckScripts failed with exit code: $LastExitCode"
   Exit $LastExitCode
 }
 
-# Do not run Node tests during CI as will already have been run in the base script
+# Do not run Node tests during CI as will already have been run in the bash script
 if ([System.Environment]::GetEnvironmentVariable("CI") -ne "true") {
   Write-Output "`n==> Start running Node tests at $(Get-Date)"
-  npm run test
+  if ($PSBoundParameters.ContainsKey('Test')) {
+    npm run test-all $Test
+  } else {
+    npm run test-all
+  }
   if ($LastExitCode) {
+    Undo-Location
     Write-Output "`nNode tests failed with exit code: $LastExitCode"
     Exit $LastExitCode
   }
 }
 
+Undo-Location
 Write-Output "`n==> Tests finished at $(Get-Date)`n"
