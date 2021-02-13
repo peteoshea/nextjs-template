47a57ce771df90b2059a2d76709a1cd560b705a2
Pete O'Shea <mail@peteoshea.co.uk>
2021-02-13 10:06:56 +0000

    Add trailing semi-colons to javascript code
    I know this isn't considered standard but I just find it easier to read.

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -1,10 +1,10 @@
-import { render, RenderResult } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react';
 // import { ThemeProvider } from "my-ui-lib"
 // import { TranslationProvider } from "my-i18n-lib"
 // import defaultStrings from "i18n/en-x-default"
 
 const Providers = ({ children }) => {
-  return children
+  return children;
   // return (
   //   <ThemeProvider theme="light">
   //     <TranslationProvider messages={defaultStrings}>
@@ -12,14 +12,14 @@ const Providers = ({ children }) => {
   //     </TranslationProvider>
   //   </ThemeProvider>
   // )
-}
+};
 
 const customRender = (ui: React.ReactElement, options = {}): RenderResult => {
-  return render(ui, { wrapper: Providers, ...options })
-}
+  return render(ui, { wrapper: Providers, ...options });
+};
 
 // re-export everything
-export * from '@testing-library/react'
+export * from '@testing-library/react';
 
 // override render method
-export { customRender as render }
+export { customRender as render };
